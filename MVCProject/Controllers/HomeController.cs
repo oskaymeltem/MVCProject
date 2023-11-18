ad871bf22ae9e525408a246b7cfc0308bedd527b
Ersah Dalganoglu <ersah.dalganoglu@monovi.com.tr>
2023-11-18 18:57:41 +0300

    Add project files.

---
+++ MVCProject/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCProject.Models;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace MVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var json = await httpClient.GetStringAsync("https://jsonplaceholder.typicode.com/users");
+                var users = JsonConvert.DeserializeObject<List<User>>(json);
+                return View(users);
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
